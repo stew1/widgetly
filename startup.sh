d05214d2f410106dec3abca71816561641cf73d0
Stewart <andrewstewart518@comcast.net>
2018-05-14 01:47:23 -0400

    silence npm i

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd client/
-npm i
+npm i --silent --production
 npm run build
 
 cd ../server/
-npm i
+npm i --silent --production
 mkdir ./public
 cp -r ../client/build/* ./public/
 node ./bin/www
