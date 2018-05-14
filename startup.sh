0fe03d02429fde2ed9ba508bde4c8215b509a525
Stewart <andrewstewart518@comcast.net>
2018-05-14 06:06:03 -0400

    reverting back to startup script

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# cd client/
-# npm i --silent --production
-# npm run build
+cd client/
+npm i --silent --production
+npm run build
 
-cd server/
+cd ../server/
 npm i --silent --production
 mkdir ./public
 cp -r ../client/build/* ./public/
