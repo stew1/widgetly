4cdd1725da1f0886d541a59c7fae5f325c17f97d
Stewart <andrewstewart518@comcast.net>
2018-05-13 20:38:18 -0400

    connection string

--- server/app.js
+++ server/app.js
@@ -30,28 +30,28 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Database connection
-// app.use(function(req, res, next) {
-//   res.locals.connection = mysql.createPool({
-//     host: 'us-cdbr-iron-east-04.cleardb.net',
-//     user: 'b786ea85e1aed3',
-//     password: '75df1ad3',
-//     database: 'heroku_b9822d41498cfcc',
-//     connectionLimit: 10,
-//   });
-//   next();
-// });
-
+// Database connection
 app.use(function(req, res, next) {
   res.locals.connection = mysql.createPool({
-    host: 'localhost',
-    user: 'widgetly-user',
-    password: 'w1dg3tly',
-    database: 'widgetly',
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b786ea85e1aed3',
+    password: '75df1ad3',
+    database: 'heroku_b9822d41498cfcc',
     connectionLimit: 10,
   });
   next();
 });
+
+// app.use(function(req, res, next) {
+//   res.locals.connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'widgetly-user',
+//     password: 'w1dg3tly',
+//     database: 'widgetly',
+//     connectionLimit: 10,
+//   });
+//   next();
+// });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
